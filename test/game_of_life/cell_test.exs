4b905d2b76d2430218ae6089bb13569436df6f47
ArturT <arturtrzop@gmail.com>
2016-04-09 13:13:22 +0200

    Ensure alive cell is on the list of alive cells in tests

--- test/game_of_life/cell_test.exs
+++ test/game_of_life/cell_test.exs
@@ -3,32 +3,32 @@ defmodule GameOfLife.CellTest do
   doctest GameOfLife.Cell
 
   test "alive cell with no neighbours dies" do
-    alive_cells = []
     alive_cell = {1, 1}
+    alive_cells = [alive_cell]
     refute GameOfLife.Cell.keep_alive?(alive_cells, alive_cell)
   end
 
   test "alive cell with 1 neighbour dies" do
-    alive_cells = [{0, 0}]
     alive_cell = {1, 1}
+    alive_cells = [alive_cell, {0, 0}]
     refute GameOfLife.Cell.keep_alive?(alive_cells, alive_cell)
   end
 
   test "alive cell with more than 3 neighbours dies" do
-    alive_cells = [{0, 0}, {1, 0}, {2, 0}, {1, 0}]
     alive_cell = {1, 1}
+    alive_cells = [alive_cell, {0, 0}, {1, 0}, {2, 0}, {1, 0}]
     refute GameOfLife.Cell.keep_alive?(alive_cells, alive_cell)
   end
 
   test "alive cell with 2 neighbours lives" do
-    alive_cells = [{0, 0}, {1, 0}]
     alive_cell = {1, 1}
+    alive_cells = [alive_cell, {0, 0}, {1, 0}]
     assert GameOfLife.Cell.keep_alive?(alive_cells, alive_cell)
   end
 
   test "alive cell with 3 neighbours lives" do
-    alive_cells = [{0, 0}, {1, 0}, {2, 1}]
     alive_cell = {1, 1}
+    alive_cells = [alive_cell, {0, 0}, {1, 0}, {2, 1}]
     assert GameOfLife.Cell.keep_alive?(alive_cells, alive_cell)
   end
 
