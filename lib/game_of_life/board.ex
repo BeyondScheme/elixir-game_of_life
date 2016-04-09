b51fa648a6bdbfeb113bc708daecdd6cde54e6cb
ArturT <arturtrzop@gmail.com>
2016-04-09 16:10:31 +0200

    Remove cells which must be killed. Add test

--- lib/game_of_life/board.ex
+++ lib/game_of_life/board.ex
@@ -3,4 +3,8 @@ defmodule GameOfLife.Board do
     alive_cells ++ new_cells
     |> Enum.uniq
   end
+
+  def remove_cells(alive_cells, kill_cells) do
+    alive_cells -- kill_cells
+  end
 end
--- test/game_of_life/board_test.exs
+++ test/game_of_life/board_test.exs
@@ -9,4 +9,13 @@ defmodule GameOfLife.BoardTest do
     expected_alive_cells = [{0, 0}, {1, 1}, {2, 2}]
     assert actual_alive_cells == expected_alive_cells
   end
+
+
+  test "remove cells which must be killed from alive cells" do
+    alive_cells = [{1, 1}, {4, -2}, {2, 2}, {2, 1}]
+    kill_cells = [{1, 1}, {2, 2}]
+    actual_alive_cells = GameOfLife.Board.remove_cells(alive_cells, kill_cells)
+    expected_alive_cells = [{4, -2}, {2, 1}]
+    assert actual_alive_cells == expected_alive_cells
+  end
 end
