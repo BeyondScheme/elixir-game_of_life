fd8ca8308535a68b72c56a252317808bda9f070b
ArturT <arturtrzop@gmail.com>
2016-04-10 16:02:17 +0200

    Add example init state for the board

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -6,7 +6,7 @@ defmodule GameOfLife do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    init_alive_cells = []
+    init_alive_cells = [{0,0}, {1,0}, {2,0}, {1,1}]
 
     children = [
       # Define workers and child supervisors to be supervised
