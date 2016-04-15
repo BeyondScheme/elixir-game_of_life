90dac8dcc48b43ad0aeed01d9f312ba69958c4aa
ArturT <arturtrzop@gmail.com>
2016-04-15 21:17:48 +0200

    Add diehard

--- lib/game_of_life/patterns/methuselah.ex
+++ lib/game_of_life/patterns/methuselah.ex
@@ -13,4 +13,15 @@ defmodule GameOfLife.Patterns.Methuselah do
       {1, 0},
     ]
   end
+
+  @doc """
+  https://en.wikipedia.org/wiki/File:Game_of_life_diehard.svg
+  """
+  def diehard do
+    [
+      {6, 2},
+      {0, 1}, {1, 1},
+      {1, 0}, {5, 0}, {6, 0}, {7, 0},
+    ]
+  end
 end
