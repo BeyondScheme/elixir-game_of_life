02c0d1d8c9ce6214a515f9258a2919b4f6ec6826
ArturT <arturtrzop@gmail.com>
2016-04-15 20:16:08 +0200

    Add lightweight_spaceship to patterns

--- lib/game_of_life/patterns/spaceships.ex
+++ lib/game_of_life/patterns/spaceships.ex
@@ -7,4 +7,14 @@ defmodule GameOfLife.Patterns.Spaceships do
       {1, 0}, {2, 0},
     ]
   end
+
+  # https://en.wikipedia.org/wiki/File:Game_of_life_animated_LWSS.gif
+  def lightweight_spaceship do
+    [
+      {2, 3}, {3, 3},
+      {0, 2}, {1, 2}, {3, 2}, {4, 2},
+      {0, 1}, {1, 1}, {2, 1}, {3, 1},
+      {1, 0}, {2, 0},
+    ]
+  end
 end
