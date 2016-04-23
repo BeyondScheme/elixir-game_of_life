c13b9de848c4f7501430ff81ea93a7be1e50d455
ArturT <arturtrzop@gmail.com>
2016-04-23 14:27:28 +0200

    Change project version to 0.1.0. Game works and all async tasks are distributed across connected nodes.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GameOfLife.Mixfile do
 
   def project do
     [app: :game_of_life,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
