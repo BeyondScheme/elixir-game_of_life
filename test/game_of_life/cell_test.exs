4812d1df7c99df48680fd497cd0393e046e41b87
ArturT <arturtrzop@gmail.com>
2016-05-09 19:46:10 +0200

    fix test - remove duplicated cell

--- test/game_of_life/cell_test.exs
+++ test/game_of_life/cell_test.exs
@@ -16,7 +16,7 @@ defmodule GameOfLife.CellTest do
 
   test "alive cell with more than 3 neighbours dies" do
     alive_cell = {1, 1}
-    alive_cells = [alive_cell, {0, 0}, {1, 0}, {2, 0}, {1, 0}]
+    alive_cells = [alive_cell, {0, 0}, {1, 0}, {2, 0}, {2, 1}]
     refute GameOfLife.Cell.keep_alive?(alive_cells, alive_cell)
   end
 
